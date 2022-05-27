98b68abd92e72f6d7e02f1e38647dcecf89c74f1
Tanya Singh <tanyasingh7068185187@gmail.com>
2022-05-27 10:08:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Inject,ScheduleComponent, Day, Week, Month, Agenda} from '@syncfusion/ej2-react-schedule';
+import {DataManager, WebApiAdaptor} from '@syncfusion/ej2-data';
+
+const remoteData=new DataManager({
+  url:'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
+  adaptor: new WebApiAdaptor,
+  crossDomain:true,
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" currentView='Month' selectedDate={new Date(2019,0,11)}>
+    <ScheduleComponent>
+    <Inject services={[Day, Week, Month, Agenda]}/>
+    </ScheduleComponent>
+
     </div>
   );
 }
